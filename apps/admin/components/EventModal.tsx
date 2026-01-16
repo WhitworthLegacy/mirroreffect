3993a50b24ac0c927c5231e5d2a926c3779f5ad4
John <ajsrl.amz@gmail.com>
2026-01-16 18:31:02 +0100

    f

--- apps/admin/components/EventModal.tsx
+++ apps/admin/components/EventModal.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import type { EventRow, PackRow } from "@/lib/adminData";
-import { useClientsStore } from "@/lib/clientsStore";
+import { useSheetsStore } from "@/lib/sheetsStore";
 
 type Props = {
   event: EventRow | null;
